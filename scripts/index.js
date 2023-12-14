1c6619765abc3c87cb587e4f3d9136f9cc7805ab
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-12-14 21:16:55 +0200

    tabs not done

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+
+  .search-container {
+    position: relative;
+    width: 353px;
+    height: 59px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #183A50;
+    box-shadow: 0px 20px 30px -8px rgba(0, 0, 0, 0.21);
+    display: flex;
+    align-items: center;
+    transition: width 0.3s ease; /* Add transition for smooth animation */
+  }
